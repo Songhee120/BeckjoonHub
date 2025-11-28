2590410e79927597e9cc8f38afab4afd9b91e40e
Songhee120 <81075657+Songhee120@users.noreply.github.com>
2025-11-28 15:39:20 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/10951. A＋B － 4/A＋B － 4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main( void )
+{
+    int a;
+    int b;
+
+    while( scanf( "%d %d" , &a ,&b ) == 2 )
+    {
+        printf( "%d\n", a + b );
+    }
+
+    return 0;
+}
+                                                 
